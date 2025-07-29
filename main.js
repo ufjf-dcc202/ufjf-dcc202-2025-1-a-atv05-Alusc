7cfdfeda6cca550ba7eb96ef47066bc547ef317d
Alusc <lucas.knopp@estudante.ufjf.br>
2025-07-29 14:51:44 -0300

    criada a pecinha

--- main.js
+++ main.js
@@ -4,8 +4,18 @@ eH1.textContent = "Olá mundo!";
 const eTabuleiro = criaTabuleiro();
 document.body.appendChild(eTabuleiro);
 
+const pecinha1 = criaPecinha("preto");
+eTabuleiro.appendChild(pecinha1);
+
 function criaTabuleiro() {
     const eTabuleiro = document.createElement("div");
     eTabuleiro.classList.add("tabuleiro");
     return eTabuleiro;
+}
+
+function criaPecinha(cor){
+    const novaPecinha = document.createElement("div");
+    novaPecinha.classList.add("pecinha");
+    novaPecinha.dataset.cor = cor;
+    return novaPecinha;
 }
